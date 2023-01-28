33b1071adce79ed4c4c7eaf3401bf41a76ac1c55
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-01-28 11:47:29 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
 const fs = require("fs");
 const Logger = require("./Logger");
-const { Client, GatewayIntentBits, Routes } = require("discord.js");
+const { Client, GatewayIntentBits, Routes, Collection } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const { loadCommands } = require("./handlers/commandHandler");
+const { loadEvents } = require("./handlers/eventHandler");
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -9,29 +14,13 @@ const client = new Client({
         GatewayIntentBits.MessageContent,
     ]
 });
-const { REST } = require("@discordjs/rest");
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
-const { loadCommands } = require("./handlers/commandHandler");
+client.commands = new Collection();
 
-Logger.Info("Starting EndlessBot...");
-
-// Loads event listeners
-Logger.Info("[SETUP] Registering events...");
-const eventFiles = fs.readdirSync('./src/listeners').filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-    Logger.Info("[SETUP] Loading event listener "+file);
-	const event = require(`./listeners/${file}`);
-	if (event.once)
-		client.once(event.name, (...args) => event.run(...args));
-	else
-		client.on(event.name, (...args) => event.run(...args));
-}
-
-// Slash command setup and bot startup
 async function main() {
     try {
+        Logger.Info("Starting EndlessBot...");
         client.login(process.env.TOKEN).then(() => {
+            loadEvents(client);
             loadCommands(client);
         });
     } catch(err) {}
