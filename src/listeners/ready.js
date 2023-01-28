b8ebd0f5fbb7ae42037398da824f3d8ae6b9a016
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-01-28 12:03:22 +0100

    Update ready.js

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,10 +1,21 @@
 const Logger = require("../Logger");
+const { ActivityType } = require('discord.js');
 
 module.exports = {
     name: 'ready',
     once: true,
-    run: function()
+    execute(client)
     {
-        Logger.Success("[INFO] Bot is now online!");
-    }
-}
+        Logger.Success(`[INFO] Bot is now online! Logged in as ${client.user.username}`);
+
+        client.user.setPresence({
+            activities: [
+                {
+                    name: "discord.io/endlessnet",
+                    type: ActivityType.Playing
+                }
+            ],
+            status: "online",
+        });
+    },
+};
