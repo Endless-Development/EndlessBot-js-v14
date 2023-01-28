383ff72ac6410318063af16425a9ea2950c5e354
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-01-28 11:52:14 +0100

    Update interactionCreate.js

--- src/listeners/interactionCreate.js
+++ src/listeners/interactionCreate.js
@@ -1,11 +1,17 @@
 const Logger = require("../Logger");
+const { CommandInteraction } = require('discord.js');
 
 module.exports = {
     name: 'interactionCreate',
-    run: function(interaction)
+    execute(interaction, client)
     {
-        if (interaction.isChatInputCommand()) {
-            Logger.Info("interaction command");
+        if (interaction.isChatInputCommand())
+        {
+            const command = client.commands.get(interaction.commandName);
+            if (!command)
+                return interaction.reply({ content: `Invalid command: ${interaction.commandName}`, ephemeral: true});
+            
+            command.execute(interaction, client);
         }
     }
 }
