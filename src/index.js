9b782a6b7162922e5e76558b9e8b3ee6f0b678ba
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-01-28 11:30:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const fs = require("fs");
-const Logger = require("./src/Logger");
+const Logger = require("./Logger");
 const { Client, GatewayIntentBits, Routes } = require("discord.js");
 const client = new Client({
     intents: [
@@ -11,44 +11,29 @@ const client = new Client({
 });
 const { REST } = require("@discordjs/rest");
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const { loadCommands } = require("./handlers/commandHandler");
 
-Logger.Info("Starting bot...");
+Logger.Info("Starting EndlessBot...");
 
 // Loads event listeners
-const eventFiles = fs.readdirSync('./src/listeners').filter(file => file.endsWith('.js'));
 Logger.Info("[SETUP] Registering events...");
-for (const file of eventFiles) {
-	const event = require(`./src/listeners/${file}`);
-    Logger.Info("[SETUP] Loading event listener "+file);
-	if (event.once) {
-		client.once(event.name, (...args) => event.run(...args));
-	} else {
-		client.on(event.name, (...args) => event.run(...args));
-	}
-}
+const eventFiles = fs.readdirSync('./src/listeners').filter(file => file.endsWith('.js'));
 
-// Loads commands
-var commands = [];
-const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
-Logger.Info("[SETUP] Loading commands...");
-for (const file of commandFiles) {
-	const command = require(`./src/commands/${file}`);
-    Logger.Info("[SETUP] Loading commands "+file);
-	commands.push({
-        name: command.name,
-        description: command.description,
-    })
+for (const file of eventFiles) {
+    Logger.Info("[SETUP] Loading event listener "+file);
+	const event = require(`./listeners/${file}`);
+	if (event.once)
+		client.once(event.name, (...args) => event.run(...args));
+	else
+		client.on(event.name, (...args) => event.run(...args));
 }
 
 // Slash command setup and bot startup
 async function main() {
     try {
-        Logger.Info("[SETUP] Loading Slash Commands");
-        Routes.applicationGuildCommand
-        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
-            body: commands,
+        client.login(process.env.TOKEN).then(() => {
+            loadCommands(client);
         });
-        client.login(process.env.TOKEN);
     } catch(err) {}
 }
 
